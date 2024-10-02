45447f81406931ab0999eaa57a45b83710538635
Lucas Labruna <lucaspablolabruna@gmail.com>
2024-10-02 17:23:51 +0000

    Created controller, router and service

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const express = require('express');
-const path = require('path')
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
-
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
-});
+const port = parseInt(process.env.PORT) || process.argv[3] || 8080
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${port}`)
 })
